92a415571a8f3f1ed92d905bd0b2f4dc3f829f63
Jan de Vries <jan@jan-v.nl>
2019-03-29 14:42:33 +0100

    Added the initial HTTP binding implementation

---
+++ HttpBinding/HttpBinding.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using HttpBinding.HttpCommand;
+using Microsoft.Azure.WebJobs.Description;
+using Microsoft.Azure.WebJobs.Host.Config;
+
+namespace HttpBinding
+{
+    [Extension(nameof(HttpBinding))]
+    public class HttpBinding : IExtensionConfigProvider
+    {
+        internal static HttpClient HttpClient;
+
+        public void Initialize(ExtensionConfigContext context)
+        {
+            var ruleQuery = context.AddBindingRule<HttpAttribute>();
+            var ruleCommand = context.AddBindingRule<HttpCommandAttribute>();
+            ruleQuery.BindToInput<HttpModel>(BuildFromAttribute);
+            ruleCommand.BindToCollector<HttpCommand.HttpCommand>(attribute => new HttpCommandAsyncCollector(attribute));
+        }
+
+        private async Task<HttpModel> BuildFromAttribute(
+            HttpAttribute attribute,
+            CancellationToken cancellationToken)
+        {
+            if (HttpBinding.HttpClient == null)
+            {
+                HttpBinding.HttpClient = attribute.HttpClient ?? new HttpClient();
+            }
+
+            var response = await HttpClient.GetAsync(attribute.Url, cancellationToken);
+
+            return new HttpModel
+            {
+                Response = await response.Content.ReadAsStringAsync(),
+                ResponseCode = (int)response.StatusCode
+            };
+        }
+    }
+}
